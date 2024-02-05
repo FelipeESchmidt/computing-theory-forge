11012a9a562242616ad2282852f2ea7176508115
Felipe E. Schmidt <schmidtfe2@gmail.com>
2024-02-05 20:55:27 -0300

    feat: creating app component and adding necessary providers to theme work correctly

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
+import { theme, themeOptions } from "./assets/styles/themes";
+import { ThemeControllerProvider } from "./contexts/ThemeControllerProvider";
 import { Router } from "./routes";
+
+const App = () => {
+  const [themeMode, setThemeMode] = React.useState<themeOptions>("dark");
+
+  return (
+    <ThemeControllerProvider setThemeMode={setThemeMode} themeMode={themeMode}>
+      <ThemeProvider theme={theme[themeMode]}>
+        <Router />
+      </ThemeProvider>
+    </ThemeControllerProvider>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 );
