98c03f8f85b308bc8755eabce1d1b414d10d8aa7
FelipeESchmidt <schmidtfe2@gmail.com>
2024-02-28 19:40:15 -0300

    feat: creating useResponsive hook

---
+++ src/hooks/useResponsive.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { breakpoint } from "../assets/styles/breakpoints";
+
+export const useResponsive = () => {
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth <= breakpoint);
+
+  const handleResize = () => setIsMobile(window.innerWidth <= breakpoint);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
