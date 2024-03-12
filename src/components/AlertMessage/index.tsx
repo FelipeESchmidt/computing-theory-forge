119a4e8168d62db605060902db1dbe3026f49ac7
FelipeESchmidt <schmidtfe2@gmail.com>
2024-03-11 21:30:59 -0300

    feat: bettering alert message styles and adding close button with handlers to clear timeout

--- src/components/AlertMessage/index.tsx
+++ src/components/AlertMessage/index.tsx
@@ -1,26 +1,45 @@
 import { hideMessage } from "@redux/AlertMessage/actions";
 import { selectAlert } from "@redux/AlertMessage/selectors";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 
-import { StyledAlert, StyledSnackbar } from "./styles";
+import { StyledAlert, StyledCloseButton, StyledSnackbar } from "./styles";
 
 function AlertMessage() {
   const dispatch = useDispatch();
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
 
   const { open, type, message } = useSelector(selectAlert);
 
+  const clearTimer = () => {
+    setTimer(undefined);
+    if (timer) {
+      clearTimeout(timer);
+    }
+  };
+
+  const handleClose = () => {
+    clearTimer();
+    dispatch(hideMessage());
+  };
+
   useEffect(() => {
     if (open) {
-      setTimeout(() => {
+      const t = setTimeout(() => {
         dispatch(hideMessage());
       }, 5000);
+      setTimer(t);
     }
+    return clearTimer;
   }, [open, dispatch]);
 
   return (
     <StyledSnackbar data-open={open}>
       <StyledAlert type={type}>{message}</StyledAlert>
+      <StyledCloseButton onClick={handleClose}>
+        <FaTimes />
+      </StyledCloseButton>
     </StyledSnackbar>
   );
 }
--- src/components/AlertMessage/styles.ts
+++ src/components/AlertMessage/styles.ts
@@ -4,25 +4,48 @@ import styled from "styled-components";
 export const StyledSnackbar = styled.div`
   &[data-open="true"] {
     opacity: 1;
-    top: 0;
+    top: 16px;
   }
   &[data-open="false"] {
     opacity: 0;
     top: -50px;
   }
   position: fixed;
-  left: 0;
-  right: 0;
+  left: 20%;
+  right: 20%;
+  font-size: 14px;
   text-align: center;
   white-space: pre-line;
-  transition-duration: 1s;
+  transition-duration: 0.75s;
+  transition-timing-function: ease-out;
   z-index: 5;
+
+  @media (max-width: 768px) {
+    left: 10%;
+    right: 10%;
+  }
 `;
 
 export const StyledAlert = styled.div<{ type: AlertMessageType }>`
-  padding: 20px;
-  color: ${({ theme }) => theme.realBlack};
-  text-shadow: 0 0 2px ${({ theme }) => theme.constant.realBlack};
+  padding: 16px;
+  padding-inline: 32px;
+  color: ${({ theme }) => theme.constant.realBlack};
   background: ${({ theme, type }) => theme.primary[type]};
-  box-shadow: 0 0 2px 1px ${({ theme }) => theme.constant.realBlack};
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.constant.realBlack};
+`;
+
+export const StyledCloseButton = styled.div`
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 12px;
+  align-items: center;
+  cursor: pointer;
+  color: ${({ theme }) => theme.constant.realBlack};
+  &:hover {
+    transform: scale(1.2);
+    transition-duration: 0.2s;
+  }
 `;
