35d0de59640b8d41daf0db54d9cd758962139bf1
Felipe E. Schmidt <schmidtfe2@gmail.com>
2024-03-14 19:50:55 -0300

    feat: adding possibility to hide bottom border

--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -2,20 +2,23 @@ import styled from "styled-components";
 
 export const StyledTable = styled.table`
   width: 100%;
-  margin: 10px 0;
   border-collapse: collapse;
   border-radius: 10px;
   background-color: #4444;
 `;
 
-export const StyledTableHead = styled.thead`
+export const StyledTableHead = styled.thead<{ hideBorder?: boolean }>`
   border-bottom: 3px solid ${({ theme }) => theme.secondary.background};
+  border-bottom-width: ${({ hideBorder }) => (hideBorder ? "0" : "3px")};
 `;
 
 export const StyledTableBody = styled.tbody``;
 
 export const StyledTableRow = styled.tr`
   border-bottom: 1px solid ${({ theme }) => theme.secondary.background};
+  &:last-child {
+    border: none;
+  }
   th:first-child {
     border-left: none;
   }
