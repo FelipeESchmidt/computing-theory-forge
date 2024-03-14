77924663164925e85739f80c0a15b9da48521e7d
Felipe E. Schmidt <schmidtfe2@gmail.com>
2024-03-14 19:41:01 -0300

    chore: adding assets in global paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
 
-const alias = ["components", "hooks", "pages", "redux", "utils", "globalTypes"];
+const alias = ["components", "assets", "hooks", "pages", "redux", "utils", "globalTypes"];
 
 // https://vitejs.dev/config/
 export default defineConfig({
