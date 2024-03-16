b97b3442419cbe6d5f22023742dbfd9c5a6d1db1
FelipeESchmidt <schmidtfe2@gmail.com>
2024-03-16 02:17:07 -0300

    feat: creating Select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+
+import * as S from "./styles";
+
+export interface SelectProps {
+  placeholder: string;
+  value: string;
+  styleType?: S.SelectStyleTypes;
+  options: { value: string; label: string }[];
+  onChange: (value: string) => void;
+  maxOptionsRendered?: number;
+}
+
+export const Select: React.FC<SelectProps> = ({
+  onChange,
+  options,
+  value,
+  placeholder,
+  styleType = "normal",
+  maxOptionsRendered = 5,
+}) => {
+  const optionsRef = React.useRef<HTMLDivElement>(null);
+  const [open, setOpen] = React.useState(false);
+  const [itemHeight, setItemHeight] = React.useState(0);
+
+  const labelSelected = options.find((option) => option.value === value)?.label;
+
+  const filteredOptions = options.filter((option) => option.value !== value);
+
+  const handleSelectOption = (value: string) => {
+    onChange(value);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (optionsRef.current) {
+      const item = optionsRef.current.children[0] as HTMLElement;
+      setItemHeight(item.clientHeight);
+    }
+  }, [optionsRef]);
+
+  return (
+    <S.StyledSelectWrapper>
+      <S.StyledSelect type={styleType} onClick={() => setOpen(!open)}>
+        {labelSelected || placeholder}
+        {open ? <FiChevronUp /> : <FiChevronDown />}
+      </S.StyledSelect>
+      <S.StyledOptions
+        ref={optionsRef}
+        open={open}
+        style={{ maxHeight: itemHeight * maxOptionsRendered }}
+      >
+        {filteredOptions.map((option) => (
+          <S.StyledOption
+            open={open}
+            type={styleType}
+            key={option.value}
+            value={option.value}
+            selected={option.value === value}
+            onClick={() => handleSelectOption(option.value)}
+          >
+            {option.label}
+          </S.StyledOption>
+        ))}
+      </S.StyledOptions>
+    </S.StyledSelectWrapper>
+  );
+};
---
+++ src/components/Select/styles.ts
@@ -0,0 +1,88 @@
+import { styled } from "styled-components";
+
+export type SelectStyleTypes = "thin" | "normal";
+
+export interface StyledSelectComponentProps {
+  type: SelectStyleTypes;
+}
+
+export interface StyledSelectComponentOpenProps {
+  open: boolean;
+}
+
+const getPadding = (type: SelectStyleTypes) => {
+  if (type === "thin") {
+    return "8px";
+  }
+  return "12px";
+};
+
+export const StyledSelectWrapper = styled.div`
+  position: relative;
+`;
+
+export const StyledSelect = styled.div<StyledSelectComponentProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${({ type }) => getPadding(type)};
+  border: 1px solid ${({ theme }) => theme.principal.text};
+  border-radius: 4px;
+  color: ${({ theme }) => theme.principal.text};
+  cursor: pointer;
+`;
+
+export const StyledOptions = styled.div<StyledSelectComponentOpenProps>`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  overflow-y: auto;
+  background-color: ${({ theme }) => theme.principal.background};
+  border: 1px solid ${({ theme }) => theme.principal.text};
+  border-top: none;
+  border-radius: 0 0 4px 4px;
+
+  opacity: ${({ open }) => (open ? 1 : 0)};
+  z-index: ${({ open }) => (open ? 3 : -1)};
+
+  &::-webkit-scrollbar {
+    width: 15px;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 2px ${({ theme }) => theme.principal.text};
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.brand.primary};
+    border-radius: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.brand.dark};
+  }
+
+  &::-webkit-resizer {
+    display: none;
+  }
+`;
+
+export const StyledOption = styled.option<
+  StyledSelectComponentProps & StyledSelectComponentOpenProps
+>`
+  transition-duration: 0.2s;
+  padding: ${({ type }) => getPadding(type)};
+  cursor: pointer;
+  color: ${({ theme }) => theme.principal.text};
+
+  pointer-events: ${({ open }) => (open ? "all" : "none")};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.principal.text};
+    color: ${({ theme }) => theme.principal.background};
+  }
+`;
