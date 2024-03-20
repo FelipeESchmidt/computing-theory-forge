644ed8a1fcc476220d5df1d38d364ba0bc603042
FelipeESchmidt <schmidtfe2@gmail.com>
2024-03-19 23:57:48 -0300

    feat: creating Tooltip component

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,32 @@
+import { useClickOutside } from "@hooks/useClickOutside";
+import React, { useRef, useState } from "react";
+import { AiOutlineClose, AiOutlineQuestionCircle } from "react-icons/ai";
+
+import * as S from "./styles";
+
+export interface TooltipProps {
+  children: React.ReactNode;
+}
+
+export const Tooltip: React.FC<TooltipProps> = ({ children }) => {
+  const childrenRef = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useClickOutside(childrenRef, () => setShow(false));
+
+  return (
+    <S.StyledTooltip>
+      <S.StyledTooltipIcon>
+        <AiOutlineQuestionCircle onClick={() => setShow(!show)} />
+      </S.StyledTooltipIcon>
+      <S.StyledTooltipContentWrapper show={show}>
+        <S.StyledTooltipContent ref={childrenRef}>
+          <S.StyleTooltipCloseButton onClick={() => setShow(false)}>
+            <AiOutlineClose />
+          </S.StyleTooltipCloseButton>
+          {children}
+        </S.StyledTooltipContent>
+      </S.StyledTooltipContentWrapper>
+    </S.StyledTooltip>
+  );
+};
---
+++ src/components/Tooltip/styles.ts
@@ -0,0 +1,51 @@
+import { styled } from "styled-components";
+
+export const StyledTooltip = styled.div``;
+
+export const StyledTooltipIcon = styled.div`
+  font-size: 16px;
+
+  > svg {
+    color: ${({ theme }) => theme.principal.text};
+    transition: color 0.3s;
+    cursor: pointer;
+    &:hover {
+      color: ${({ theme }) => theme.indicators.info};
+    }
+  }
+`;
+
+export const StyledTooltipContentWrapper = styled.div<{ show: boolean }>`
+  position: fixed;
+  inset: 0;
+  transition: opacity 0.3s;
+  background-color: ${({ theme }) => `${theme.principal.background}88`};
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  pointer-events: ${({ show }) => (show ? "all" : "none")};
+  z-index: 100;
+`;
+
+export const StyledTooltipContent = styled.div`
+  position: fixed;
+  inset: 20%;
+  border-radius: 8px;
+  padding: 12px;
+  overflow-y: auto;
+  border: 1px solid ${({ theme }) => theme.principal.text};
+  background-color: ${({ theme }) => theme.principal.background};
+`;
+
+export const StyleTooltipCloseButton = styled.div`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  cursor: pointer;
+  font-size: 16px;
+  color: ${({ theme }) => theme.principal.text};
+
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${({ theme }) => theme.indicators.danger};
+  }
+`;
