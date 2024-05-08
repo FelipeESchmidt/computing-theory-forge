a7eebf930f9c6e830c689aa57c1b6babbb2d89f4
FelipeESchmidt <schmidtfe2@gmail.com>
2024-05-07 23:44:12 -0300

    fix: clearing header steps when enter the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import { Container } from "@components/Container";
 import { Header } from "@components/Header";
+import { useHeaderController } from "@contexts/HeaderProvider";
+import { useEffect } from "react";
 
 import { products } from "./constants";
 import * as S from "./styles";
 
 export const Home: React.FC = () => {
+  const { setSteps } = useHeaderController();
+
+  useEffect(() => {
+    setSteps([]);
+  }, []);
+
   return (
     <S.StyledContainer>
       <Header />
